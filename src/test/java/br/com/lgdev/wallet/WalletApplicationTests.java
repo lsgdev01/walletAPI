8b866cf035dc532c321ea96f9e9ddc607cb76527
lsgdev01 <74672309+lsgdev01@users.noreply.github.com>
2021-06-16 02:38:02 -0300

    initial commit

---
+++ src/test/java/br/com/lgdev/wallet/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lgdev.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
